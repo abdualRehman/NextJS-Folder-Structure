669021f317d9b99e00e6ea3b549af7d900cbe9ce
Abdual Rehman <rehmanali00708@gmail.com>
2023-11-05 18:11:15 +0500

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { Main } from '@/components/Main';
 import * as S from './styles';
 
 export default function Home() {
   return (
     <S.Wrapper>
+      <Main />
+      <h1>Next 13 boilerplate</h1>
+      <h1>Next 14 boilerplate</h1>
+      <h1>Next 13 boilerplate</h1>
+      <h1>Next 14 boilerplate</h1>
       <h1>Next 13 boilerplate</h1>
       <h1>Next 14 boilerplate</h1>
     </S.Wrapper>
