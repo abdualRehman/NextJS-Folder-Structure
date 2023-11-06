a7ff6088e36430289267b34fc26c915ec3cf0631
abdulrehman_ <abdul.rahman@nexrupt.com>
2023-11-06 13:55:57 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { Main } from '@/components/Main';
-import * as S from './styles';
+import * as S from "./styles";
 
 export default function Home() {
   return (
     <S.Wrapper>
       <Main />
-      <h1>Next 13 boilerplate</h1>
-      <h1>Next 14 boilerplate</h1>
+      <h1
+      >Next 13 boilerplate
+
+      </h1>
+      <h1>Next 14 Test 1</h1>
       <h1>Next 13 boilerplate</h1>
       <h1>Next 14 boilerplate</h1>
       <h1>Next 13 boilerplate</h1>
