e1a3726f7880009f7171aee2b1d1bd786f7772a1
abdulrehman_ <abdul.rahman@nexrupt.com>
2023-11-06 18:15:36 +0500

    index file updated2

--- src/app/lib/registry.tsx
+++ src/app/lib/registry.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 'use client';
 
-import React, { useState, ReactNode } from 'react';
+import React, { useState } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode;
 }) {
+  // Only create stylesheet once with lazy initial state
+  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   useServerInsertedHTML(() => {
@@ -27,32 +29,3 @@ export default function StyledComponentsRegistry({
     </StyleSheetManager>
   );
 }
-
-// import React, { useState } from 'react';
-// import { useServerInsertedHTML } from 'next/navigation';
-// import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
-
-// export default function StyledComponentsRegistry({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   // Only create stylesheet once with lazy initial state
-//   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-//   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
-
-//   useServerInsertedHTML(() => {
-//     const styles = styledComponentsStyleSheet.getStyleElement();
-//     //@ts-ignore
-//     styledComponentsStyleSheet.instance.clearTag();
-//     return <>{styles}</>;
-//   });
-
-//   if (typeof window !== 'undefined') return <>{children}</>;
-
-//   return (
-//     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-//       {children}
-//     </StyleSheetManager>
-//   );
-// }
